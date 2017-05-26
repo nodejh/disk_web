637240f54d25801ce564248e24979adb239083ce
nodejh <jianghangscu@gmail.com>
2017-05-26 22:38:27 +0800

    debug

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -2,29 +2,40 @@ const mongoose = require('mongoose');
 const config = require('./../config/config');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.mongodb);
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-mongoose.connection.once('open', () => {
-  console.log('连接数据库成功');
-  console.log('MongoDB event open');
-  console.log('MongoDB connected [%s]', config.mongodb);
+// mongoose.connect(config.mongodb);
+// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connection.once('open', () => {
+//   console.log('连接数据库成功');
+//   console.log('MongoDB event open');
+//   console.log('MongoDB connected [%s]', config.mongodb);
+//
+//   mongoose.connection.on('connected', function() {
+//     console.log('MongoDB event connected');
+//   });
+//
+//   mongoose.connection.on('disconnected', function() {
+//     console.log('MongoDB event disconnected');
+//   });
+//
+//   mongoose.connection.on('reconnected', function() {
+//     console.log('MongoDB event reconnected');
+//   });
+//
+//   mongoose.connection.on('error', function(err) {
+//     console.error('MongoDB event error: ' + err);
+//   });
+// });
 
-  mongoose.connection.on('connected', function() {
-    console.log('MongoDB event connected');
+const connectWithRetry = () => {
+  console.log('connecting...');
+  return mongoose.connect(config.mongodb, (err) => {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    }
   });
-
-  mongoose.connection.on('disconnected', function() {
-    console.log('MongoDB event disconnected');
-  });
-
-  mongoose.connection.on('reconnected', function() {
-    console.log('MongoDB event reconnected');
-  });
-
-  mongoose.connection.on('error', function(err) {
-    console.error('MongoDB event error: ' + err);
-  });
-});
+};
+connectWithRetry();
 
 
 module.exports = mongoose;
